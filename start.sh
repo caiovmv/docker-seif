690596c9174ea9b738424e68875af679adbb54d1
caiovmv <villela.caio@gmail.com>
2017-12-11 21:48:52 -0200

    Disable MySQL Service

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 cd /opt/seif
 
-service mysql start && service nginx start && java -jar safety-1.0-SNAPSHOT.jar && java -jar auth-1.0-SNAPSHOT.jar 
+service nginx start && java -jar safety-1.0-SNAPSHOT.jar && java -jar auth-1.0-SNAPSHOT.jar 
